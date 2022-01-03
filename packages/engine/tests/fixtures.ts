34fb1d573b0f0a0007df29039094a728eae0d7c9
Fotis Alexandrou <falexandrou@gmail.com>
2022-01-03 02:15:09 +0200

    fix output path in tests

--- packages/engine/tests/fixtures.ts
+++ packages/engine/tests/fixtures.ts
@@ -1,13 +1,12 @@
 import os from 'os';
 import faker from 'faker';
-import { join as joinPaths } from 'path';
 
 import { ProjectConfiguration } from '@stackmate/types';
 import { SERVICE_TYPE } from '@stackmate/constants';
 import { AWS_REGIONS, RDS_INSTANCE_SIZES } from '@stackmate/clouds/aws/constants';
 
 export const stackName = `test-stack-${faker.random.alphaNumeric(12)}`;
-export const outputPath = joinPaths(os.tmpdir(), 'output-files', faker.datatype.hexaDecimal(12));
+export const outputPath = os.tmpdir();
 export const awsRegion = faker.random.arrayElement(Object.values(AWS_REGIONS)) as string;
 export const awsKeyArn = `arn:aws:kms:${awsRegion}:111122223333:key/1234abcd-12ab-34cd-56ef-1234567890ab`;
 
