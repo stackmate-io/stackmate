7e7dfd34af2e18e4baa39dc0ef8a047c2f5192cc
Fotis Alexandrou <falexandrou@gmail.com>
2022-01-21 00:16:30 +0200

    scaffold cli / bindings

---
+++ packages/cli/bindings/terraform.go
@@ -0,0 +1,14 @@
+package main
+
+import "C"
+
+// export Create
+func Create() {}
+
+// export Deploy
+func Deploy() {}
+
+// export State
+func State() {}
+
+func main() {}
---
+++ packages/cli/src/constants.ts
@@ -0,0 +1,3 @@
+import path from 'node:path';
+
+export const TERRAFORM_BINDING = path.resolve(path.join('bindings', 'terraform.so'));
