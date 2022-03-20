3813571f0d69d04617590ec6aabdb05eab7f4a6e
Fotis Alexandrou <falexandrou@gmail.com>
2022-03-20 03:50:36 +0200

    resolve the correct bindings, commit shared library, fix make path

--- packages/cli/src/lib/terraform/bindings.go
+++ packages/cli/src/lib/terraform/bindings.go
@@ -2,7 +2,46 @@ package main
 
 import "C"
 
-// export Execute
-func Execute() {}
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"github.com/hashicorp/go-version"
+	"github.com/hashicorp/hc-install/product"
+	"github.com/hashicorp/hc-install/releases"
+	"github.com/hashicorp/terraform-exec/tfexec"
+)
+
+// export Apply
+func Apply() {
+	installer := &releases.ExactVersion{
+		Product: product.Terraform,
+		Version: version.Must(version.NewVersion("1.0.6")),
+	}
+
+	execPath, err := installer.Install(context.Background())
+	if err != nil {
+		log.Fatalf("error installing Terraform: %s", err)
+	}
+
+	workingDir := "/path/to/working/dir"
+	tf, err := tfexec.NewTerraform(workingDir, execPath)
+	if err != nil {
+		log.Fatalf("error running NewTerraform: %s", err)
+	}
+
+	err = tf.Init(context.Background(), tfexec.Upgrade(true))
+	if err != nil {
+		log.Fatalf("error running Init: %s", err)
+	}
+
+	state, err := tf.Show(context.Background())
+	if err != nil {
+		log.Fatalf("error running Show: %s", err)
+	}
+
+	fmt.Println(state.FormatVersion) // "0.1"
+}
 
 func main() {}
