40cd3fbc57c96636c9303ddf13acce61ee126f66
Fotis Alexandrou <falexandrou@gmail.com>
2022-05-18 20:48:33 +0300

    wip: cleanup

--- packages/engine/src/core/project.ts
+++ packages/engine/src/core/project.ts
@@ -1,12 +1,10 @@
 import Entity from '@stackmate/engine/lib/entity';
-import Parser from '@stackmate/engine/lib/parsers';
-import { PROVIDER } from '@stackmate/engine/constants';
 import { Attribute } from '@stackmate/engine/lib/decorators';
 import { normalizeProject } from '@stackmate/engine/lib/normalizers';
 import {
   ProjectConfiguration, NormalizedProjectConfiguration, StackmateProject,
-  StagesNormalizedAttributes, Validations, StateConfiguration,
-  AttributeParsers, VaultConfiguration, ProviderChoice,
+  StagesNormalizedAttributes, StateConfiguration,
+  VaultConfiguration, ProviderChoice,
 } from '@stackmate/engine/types';
 
 class Project extends Entity implements StackmateProject {
@@ -39,72 +37,6 @@ class Project extends Entity implements StackmateProject {
    * @var {Object} stages the stages declarations
    */
   @Attribute stages: StagesNormalizedAttributes = {};
-
-  /**
-   * @var {String} validationMessage the error message
-   */
-  readonly validationMessage: string = 'The project’s configuration is not valid';
-
-  /**
-   * Returns a list of validations to validate the structure of the configuration file with
-   *
-   * @returns {Validations} the list of validations to use for the config file
-   */
-  validations(): Validations {
-    const providers = Object.values(PROVIDER);
-
-    return {
-      name: {
-        presence: {
-          allowEmpty: false,
-          message: 'You have to provide a name for the project',
-        },
-        format: {
-          pattern: '[a-z0-9-_./]+',
-          flags: 'i',
-          message: 'The project name needs to be in URL-friendly format, same as the repository name',
-        },
-      },
-      provider: {
-        presence: {
-          message: 'A default cloud provider should be specified',
-        },
-        inclusion: {
-          within: providers,
-          message: `The default cloud provider is invalid. Available options are ${providers.join(', ')}`,
-        },
-      },
-      region: {
-        presence: {
-          message: 'A default region (that corresponds to the regions that the default cloud provider provides) should be specified',
-        },
-      },
-      secrets: {
-        validateSecrets: {},
-      },
-      stages: {
-        validateStages: {},
-      },
-      state: {
-        validateState: {},
-      },
-    };
-  }
-
-  /**
-   * @returns {AttributeParsers} the functions to parse the attributes with
-   */
-  parsers(): AttributeParsers {
-    return {
-      name: Parser.parseString,
-      region: Parser.parseString,
-      provider: Parser.parseString,
-      secrets: Parser.parseObject,
-      state: Parser.parseObject,
-      stages: Parser.parseObject,
-      defaults: Parser.parseObject,
-    };
-  }
 
   /**
    * Applies arguments in stage services that were skipped for brevity
