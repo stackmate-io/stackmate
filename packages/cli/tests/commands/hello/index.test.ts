8531781986574e0ecc401c3abcc9f79d70c555df
Fotis Alexandrou <falexandrou@gmail.com>
2022-06-16 15:17:20 +0300

    cleanup: obsolete packages, tests

--- packages/cli/tests/commands/hello/index.test.ts
+++ packages/cli/tests/commands/hello/index.test.ts
@@ -1,17 +1,21 @@
 import InitCommand from '@stackmate/cli/commands/init';
 
 describe('Init Command', () => {
-  let results: any = [];
+  let results: string = '';
 
   beforeEach(() => {
-    results = [];
-    jest.spyOn(process.stdout, 'write').mockImplementation(val => results.push(val));
+    results = '';
+
+    jest.spyOn(process.stdout, 'write').mockImplementation(val => {
+      results += val.toString();
+      return true;
+    });
   });
 
   afterEach(() => jest.restoreAllMocks());
 
   it('should temporarily do nothing', async () => {
     await InitCommand.run([]);
-    expect(results).toContain('Test')
+    expect(results).toContain('Test');
   });
 });
