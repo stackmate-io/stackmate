10c8333c62a7b25a55046de862f6e7f65074bf11
Fotis Alexandrou <falexandrou@gmail.com>
2022-08-23 01:06:36 +0300

    chore: add tests for project.stage method

--- packages/engine/tests/core/project.test.ts
+++ packages/engine/tests/core/project.test.ts
@@ -1,18 +1,70 @@
 import { cloneDeep } from 'lodash';
 import { deepEqual } from 'assert';
+import {
+  BaseService, ProjectConfiguration, PROVIDER, SERVICE_TYPE, StackmateProject,
+} from '@stackmate/engine';
 import Project from '@stackmate/engine/core/project';
-import { awsMinimalDatabaseState } from 'tests/engine/fixtures/configs';
+
+const projectFixture: ProjectConfiguration = {
+  name: 'some-project',
+  provider: 'aws',
+  region: 'eu-central-1',
+  state: {
+    bucket: 'abc-defg',
+  },
+  stages: [{
+    name: 'production',
+    services: [{
+      type: 'mysql',
+      name: 'mydatabase',
+      database: 'mysqldb',
+    }],
+  }],
+};
+
 
 describe('Project', () => {
   describe('validate', () => {
     it('validates a correct configuration', () => {
-      expect(() => Project.validate(awsMinimalDatabaseState, { useDefaults: false })).not.toThrow();
+      expect(() => Project.validate(projectFixture, { useDefaults: false })).not.toThrow();
     });
 
     it('leaves the state of a valid configuration intact', () => {
-      const source = cloneDeep(awsMinimalDatabaseState);
-      Project.validate(awsMinimalDatabaseState, { useDefaults: false });
-      expect(() => deepEqual(source, awsMinimalDatabaseState)).not.toThrow();
+      const source = cloneDeep(projectFixture);
+      Project.validate(projectFixture, { useDefaults: false });
+      expect(() => deepEqual(source, projectFixture)).not.toThrow();
     });
+  });
+
+  describe('stage population', () => {
+    let services: BaseService.Type[];
+
+    beforeAll(() => {
+      services = Project.factory<StackmateProject.Type>(projectFixture).stage('production');
+    });
+
+    it('should feature a provider service for AWS', () => (
+      expect(services.filter(
+        srv => srv.type === SERVICE_TYPE.PROVIDER && srv.provider === PROVIDER.AWS,
+      )).not.toBeNull()
+    ));
+
+    it('should feature a state service', () => (
+      expect(services.filter(
+        srv => srv.type === SERVICE_TYPE.STATE && srv.provider === PROVIDER.AWS,
+      )).not.toBeNull()
+    ));
+
+    it('should feature a vault service', () => (
+      expect(services.filter(
+        srv => srv.type === SERVICE_TYPE.VAULT && srv.provider === PROVIDER.AWS,
+      )).not.toBeNull()
+    ));
+
+    it('should feature a mysql service, according to the fixture', () => (
+      expect(services.filter(
+        srv => srv.type === SERVICE_TYPE.MYSQL && srv.provider === PROVIDER.AWS,
+      )).not.toBeNull()
+    ));
   });
 });
