2faaf2a83eda89a893a6c1e95e47b22192d75e5e
Fotis Alexandrou <falexandrou@gmail.com>
2022-09-25 16:58:23 +0300

    chore: add aws provider tests

--- packages/engine/tests/services/aws/provider.test.ts
+++ packages/engine/tests/services/aws/provider.test.ts
@@ -1,7 +1,19 @@
+import { KmsKey } from '@cdktf/provider-aws/lib/kms';
+import { AwsProvider as TerraformAwsProvider } from '@cdktf/provider-aws';
+import { InternetGateway, Subnet, Vpc } from '@cdktf/provider-aws/lib/vpc';
+
 import { AwsProvider } from '@stackmate/engine/providers';
-import { PROVIDER, SERVICE_TYPE } from '@stackmate/engine';
 import { REGIONS } from '@stackmate/engine/providers/aws/constants';
-import { ServiceScopeChoice } from '@stackmate/engine/core/service';
+import { PROVIDER, SERVICE_TYPE } from '@stackmate/engine';
+import { DEFAULT_PROFILE_NAME } from '@stackmate/engine/constants';
+import { Provisionable, ServiceScopeChoice } from '@stackmate/engine/core/service';
+import { getStack, Stack } from '@stackmate/engine/core/stack';
+import { getProvisionableFromConfig } from '@stackmate/engine/core/operation';
+import {
+  AwsProviderDeployableResources, AwsProviderAttributes, AwsProviderDeployableProvisionable,
+  AwsProviderDestroyableProvisions, AwsProviderPreparableProvisionable,
+  AwsProviderPreparableProvisions, AwsProviderDestroyableProvisionable,
+} from '@stackmate/engine/providers/aws/services/provider';
 
 describe('AWS Provider', () => {
   const service = AwsProvider;
@@ -51,9 +63,69 @@ describe('AWS Provider', () => {
     });
   });
 
-  /*
-  it('registers the service into the stack and creates the deployable provisions');
-  it('registers the service into the stack and creates the destroyable provisions');
-  it('registers the service into the stack and creates the preparable provisions');
-  */
+  describe('provision handlers', () => {
+    let stack: Stack;
+    const stage = 'a-stage';
+    let provisionable: Provisionable;
+    let config: AwsProviderAttributes;
+
+    beforeEach(() => {
+      stack = getStack('my-project', stage);
+      config = {
+        provider: 'aws',
+        type: SERVICE_TYPE.PROVIDER,
+        region: 'eu-central-1',
+        profile: DEFAULT_PROFILE_NAME,
+        overrides: {},
+      };
+
+      provisionable = getProvisionableFromConfig(config, stage);
+    });
+
+    it('registers the service into the stack and creates the deployable provisions', () => {
+      const deployHandler = service.handlers.get('deployable')!;
+      expect(typeof deployHandler === 'function').toBeTruthy();
+
+      const resources = deployHandler(
+        provisionable as AwsProviderDeployableProvisionable, stack,
+      ) as AwsProviderDeployableResources;
+
+      expect(resources).toBeInstanceOf(Object);
+      expect(new Set(Object.keys(resources))).toEqual(
+        new Set(['gateway', 'kmsKey', 'vpc', 'provider', 'subnets']),
+      );
+      expect(resources.gateway).toBeInstanceOf(InternetGateway);
+      expect(resources.kmsKey).toBeInstanceOf(KmsKey);
+      expect(resources.vpc).toBeInstanceOf(Vpc);
+      expect(resources.provider).toBeInstanceOf(TerraformAwsProvider);
+      expect(Array.isArray(resources.subnets)).toBeTruthy();
+      expect(resources.subnets.every(s => s instanceof Subnet)).toBeTruthy();
+    });
+
+    it('registers the service into the stack and creates the destroyable provisions', () => {
+      const destroyHandler = service.handlers.get('destroyable')!;
+      expect(typeof destroyHandler === 'function').toBeTruthy();
+
+      const resources = destroyHandler(
+        provisionable as AwsProviderDestroyableProvisionable, stack,
+      ) as AwsProviderDestroyableProvisions;
+
+      expect(resources).toBeInstanceOf(Object);
+      expect(new Set(Object.keys(resources))).toEqual(new Set(['provider']));
+      expect(resources.provider).toBeInstanceOf(TerraformAwsProvider);
+    });
+
+    it('registers the base resources', () => {
+      const prepareHandler = service.handlers.get('preparable')!;
+      expect(typeof prepareHandler === 'function').toBeTruthy();
+
+      const resources = prepareHandler(
+        provisionable as AwsProviderPreparableProvisionable, stack,
+      ) as AwsProviderPreparableProvisions;
+
+      expect(resources).toBeInstanceOf(Object);
+      expect(new Set(Object.keys(resources))).toEqual(new Set(['provider']));
+      expect(resources.provider).toBeInstanceOf(TerraformAwsProvider);
+    });
+  });
 });
