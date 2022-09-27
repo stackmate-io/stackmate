1f3a51a63129709d5908c8f62dae5c34f833352d
Fotis Alexandrou <falexandrou@gmail.com>
2022-09-27 10:17:12 +0300

    feat: local state & tests

--- packages/engine/src/providers/local/service.ts
+++ packages/engine/src/providers/local/service.ts
@@ -1,9 +1,15 @@
+import pipe from '@bitty/pipe';
+import { LocalProvider as TerraformLocalProvider } from '@cdktf/provider-local';
+
 import { PROVIDER, SERVICE_TYPE } from '@stackmate/engine/constants';
-import { BaseServiceAttributes, ServiceAssociation, ServiceScopeChoice } from '@stackmate/engine/core/service';
-import { LocalProviderService } from './services/provider';
+import { LocalProviderAttributes, LocalProviderProvisionable } from '@stackmate/engine/providers/local/services/provider';
+import {
+  associate, BaseServiceAttributes, getCoreService, ServiceAssociation,
+  ServiceScopeChoice, ServiceTypeChoice,
+} from '@stackmate/engine/core/service';
 
 type ProviderAssociation<S extends ServiceScopeChoice> = ServiceAssociation<
-  'providerInstance', typeof SERVICE_TYPE.PROVIDER, S, LocalProviderService
+  'providerInstance', typeof SERVICE_TYPE.PROVIDER, S, TerraformLocalProvider
 >;
 
 export type LocalServiceAssociations = [
@@ -14,6 +20,20 @@ export type LocalServiceAttributes<Attrs extends BaseServiceAttributes> = Attrs 
   provider: typeof PROVIDER.LOCAL;
 };
 
-export type LocalService<Attrs extends BaseServiceAttributes> = Attrs & {
-  provider: typeof PROVIDER.LOCAL;
-};
+const associations: LocalServiceAssociations = [{
+  as: 'providerInstance',
+  from: SERVICE_TYPE.PROVIDER,
+  scope: 'preparable',
+  where: (config: LocalProviderAttributes, linked: BaseServiceAttributes) => (
+    config.provider === linked.provider
+  ),
+  handler: (p: LocalProviderProvisionable): TerraformLocalProvider => {
+    return p.provisions.provider
+  },
+}];
+
+export const getLocalService = (type: ServiceTypeChoice) => (
+  pipe(
+    associate(associations),
+  )(getCoreService(PROVIDER.LOCAL, type))
+);
