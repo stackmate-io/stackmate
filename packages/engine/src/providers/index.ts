f33ad2c92ff074ab378ccb7093aa8e87bbab8aec
Fotis Alexandrou <falexandrou@gmail.com>
2022-10-01 20:36:23 +0300

    fix: tests

--- packages/engine/src/providers/index.ts
+++ packages/engine/src/providers/index.ts
@@ -2,7 +2,6 @@ import { DEFAULT_REGION } from '@stackmate/engine/providers/aws/constants';
 import { PROVIDER } from '@stackmate/engine/constants';
 
 export * from './services';
-export * as AttributeTypes from './attributes';
 
 export const DEFAULT_REGIONS: Record<string, string> = {
   [PROVIDER.AWS]: DEFAULT_REGION,
