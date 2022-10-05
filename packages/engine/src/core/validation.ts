d51d5f1b6ef40fd711b2ca3d3f0511c5292eaeaa
Fotis Alexandrou <falexandrou@gmail.com>
2022-10-05 18:22:16 +0300

    wip: init command & tests

--- packages/engine/src/core/validation.ts
+++ packages/engine/src/core/validation.ts
@@ -221,7 +221,7 @@ export const loadJsonSchema = (
  * Validates an attribute-set against a schema id found in the schema
  *
  * @param {String} schemaId the schema id to use for validation
- * @param {Object} data the data to validate
+ * @param {Any} data the data to validate
  * @param {AjvOptions} options any Ajv options to use
  * @returns {Object} the clean / validated attributes
  */
@@ -249,6 +249,18 @@ export const validate = (
 
   return validData;
 };
+
+/**
+ * Validates a single property in the schema
+ *
+ * @param {String} property the name of the property to validate
+ * @param {Any} data the data to validate
+ * @param {String} root the root for the property
+ * @returns {Any}
+ */
+export const validateProperty = (property: string, data: any, root = JSON_SCHEMA_ROOT): any => (
+  validate(`${root}#/properties/${property}`, data)
+);
 
 /**
  * Parses Ajv errors to custom, error descriptors
