16b9a367ac744bc9c6661f259477768850250267
Fotis Alexandrou <falexandrou@gmail.com>
2022-10-07 20:13:35 +0300

    feat: project init command

--- packages/cli/jest.config.ts
+++ packages/cli/jest.config.ts
@@ -1,14 +1,11 @@
-import path from 'path';
 import { JestConfigWithTsJest } from 'ts-jest';
 
 import config from '../../jest.config.base';
 
-const dirName = path.basename(path.resolve(__dirname));
-
 const opts: JestConfigWithTsJest = {
   ...config,
   rootDir: '../..',
-  testRegex: `packages/${dirName}/tests/.*\\.test\\.ts$`,
+  testRegex: `packages/cli/tests/.*\\.test\\.ts$`,
 };
 
 export default opts;
