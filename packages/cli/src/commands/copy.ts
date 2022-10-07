a9f77a11dc1d672259d3f432c24cb3c5444ea08a
Fotis Alexandrou <falexandrou@gmail.com>
2022-10-07 23:54:14 +0300

    cleanup: commands base, types, remove obsolete command

--- packages/cli/src/commands/copy.ts
+++ packages/cli/src/commands/copy.ts
@@ -1,36 +1,45 @@
 import { Flags } from '@oclif/core';
+import { ArgInput, FlagInput } from '@oclif/core/lib/interfaces';
 
 import BaseCommand from '@stackmate/cli/core/commands/base';
-import { parseCommaSeparatedString } from '@stackmate/cli/lib';
 
 class StageCopyCommand extends BaseCommand {
   /**
    * @var {Array} args the command's arguments
    */
-  static args = [
+  static args: ArgInput = [
     ...BaseCommand.args,
+    {
+      name: 'source',
+      description: 'The source stage to copy',
+      required: true,
+      parse: async (input: string) => (input || '').trim(),
+    },
+    {
+      name: 'target',
+      description: 'The target stage to copy',
+      required: true,
+      parse: async (input: string) => (input || '').trim(),
+    },
   ];
+
+  /**
+   * @var {String} summary the command's short description
+   */
+  static summary = 'Copies a stage to anoyther.';
 
   /**
    * @var {Object} flags the flags to use in the command
    */
-  static flags = {
+  static flags: FlagInput = {
     ...BaseCommand.flags,
-    from: Flags.string({
-      char: 'f',
-      default: 'production',
-      required: true,
-      parse: async (v: string) => parseCommaSeparatedString(v).join(','),
-    }),
-    skip: Flags.string({
-      char: 's',
-      default: '',
-      parse: async (v: string) => parseCommaSeparatedString(v).join(','),
+    full: Flags.boolean({
+      default: false,
+      required: false,
     }),
   };
 
   async run(): Promise<any> {
-    // console.log(this.parsedFlags);
   }
 }
 
