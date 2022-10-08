f3fc96230f4e4f88b0bee0672d67f21f0dc02e7c
Fotis Alexandrou <falexandrou@gmail.com>
2022-10-08 12:07:09 +0300

    feat: copy stage command

--- packages/cli/src/core/commands/base.ts
+++ packages/cli/src/core/commands/base.ts
@@ -1,8 +1,8 @@
 import { Command } from '@oclif/core';
-import { ProjectConfiguration } from '@stackmate/engine';
-import { ArgInput, FlagInput, OutputArgs } from '@oclif/core/lib/interfaces';
+import { ProjectConfiguration, StageConfiguration } from '@stackmate/engine';
+import { ArgInput, FlagInput } from '@oclif/core/lib/interfaces';
 
-import { ConfigurationFile } from '@stackmate/cli/lib';
+import { ConfigurationFile, StageNotFoundError } from '@stackmate/cli/lib';
 import { DEFAULT_PROJECT_FILE } from '@stackmate/cli/constants';
 
 abstract class BaseCommand extends Command {
@@ -22,18 +22,63 @@ abstract class BaseCommand extends Command {
   /**
    * @var {ArgInput} arguments the arguments used in the command
    */
-  protected parsedArgs: OutputArgs;
+  protected parsedArgs: { [name: string]: any };
 
   /**
    * @var {Object} flags the parsed flags
    */
-  protected parsedFlags: FlagInput;
+  protected parsedFlags: { [name: string]: any };
+
+  protected filename: string  = DEFAULT_PROJECT_FILE;
+
+  /**
+   * @var {ProjectConfiguration} config the configuration object
+   */
+  #config: ConfigurationFile;
+
+  /**
+   * @returns {ConfigurationFile} the configuration file associated with this command
+   */
+  get configFile(): ConfigurationFile {
+    if (!this.#config) {
+      this.#config = new ConfigurationFile(this.filename);
+    }
+
+    return this.#config;
+  }
 
   /**
    * @returns {ProjectConfiguration} the project configuration to load from the file
    */
   get projectConfig(): ProjectConfiguration {
-    return new ConfigurationFile(DEFAULT_PROJECT_FILE).read() as ProjectConfiguration;
+    this.assertConfigExists();
+    return this.configFile.read() as ProjectConfiguration;
+  }
+
+  /**
+   * Asserts that a configuration file exists
+   */
+  assertConfigExists() {
+    if (this.configFile.exists) {
+      return;
+    }
+
+    this.log('Stackmate configuration not found. Use the `init` command to create one');
+    this.exit(1);
+  }
+
+  /**
+   * @param {String} name the stage's name
+   * @returns {StageConfiguration} the stage requested
+   */
+  stage(name: string): StageConfiguration<true> {
+    const stage = this.projectConfig.stages?.find(stg => stg.name === name);
+
+    if (!stage) {
+      throw new StageNotFoundError(name);
+    }
+
+    return stage;
   }
 
   /**
