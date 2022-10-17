4a6db5a507e064e5768bd37f19e3525ab1cec679
Fotis Alexandrou <falexandrou@gmail.com>
2022-10-18 00:55:40 +0300

    feat: add option to provide alerts

---
+++ packages/engine/src/core/service/withAlerts.ts
@@ -0,0 +1,53 @@
+import pipe from '@bitty/pipe';
+import { isEmpty } from 'lodash';
+
+import {
+  associate, AssociationHandler, BaseServiceAttributes, Service,
+  withSchema, ServiceSideEffect, ProvisionResources,
+} from '@stackmate/engine/core/service/core';
+
+/**
+ * @type {AlertableAttributes} internal link service attributes
+ */
+export type AlertableAttributes = { alerts: { email: string; } };
+
+/**
+ * @type {ServiceLinkHandler} the function who handles service linking
+ */
+export type ServiceLinkHandler = AssociationHandler<
+  ProvisionResources, BaseServiceAttributes & AlertableAttributes
+>;
+
+/**
+ * Adds link support to a service (allows it to be linked to other services)
+ *
+ * @param {ServiceLinkHandler} onServiceLinked the function handling service links
+ * @returns {Function<Service>}
+ */
+export const withAlerts = <C extends BaseServiceAttributes>(
+  onServiceLinked: ServiceLinkHandler,
+) => <T extends Service<C>>(srv: T): T => (
+  pipe(
+    withSchema<C, AlertableAttributes>({
+      type: 'object',
+      properties: {
+        alerts: {
+          type: 'object',
+          default: {},
+          properties: {
+            email: {
+              type: 'string',
+              format: 'email',
+              description: 'The email address to send the alerts to',
+            },
+          },
+        },
+      },
+    }),
+    associate<C, ServiceSideEffect[]>([{
+      scope: 'deployable',
+      handler: onServiceLinked,
+      where: (config: C & AlertableAttributes): boolean => !isEmpty(config.alerts),
+    }]),
+  )(srv)
+);
