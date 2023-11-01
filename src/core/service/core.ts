e1fbeb0808a5a39cb77b4a3790bd7c67d4c5c4b5
Fotis Alexandrou <falexandrou@gmail.com>
2023-11-02 01:59:23 +0200

    move function to operation module

--- src/core/service/core.ts
+++ src/core/service/core.ts
@@ -427,27 +427,3 @@ export const withEnvironment =
       },
     ],
   })
-
-/**
- * @param {BaseProvisionable} provisionable the provisionable to check
- * @throws {Error} if a requirement is not satisfied
- */
-export const assertRequirementsSatisfied = (provisionable: BaseProvisionable) => {
-  const {
-    service: { associations, type },
-    requirements,
-  } = provisionable
-  if (!associations) {
-    return
-  }
-
-  Object.entries(associations).forEach(([name, assoc]) => {
-    if (!assoc.requirement) {
-      return
-    }
-
-    if (!requirements[name]) {
-      throw new Error(`Requirement ${name} for service ${type} is not satisfied`)
-    }
-  })
-}
