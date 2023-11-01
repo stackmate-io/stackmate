5b32c820dedc6a7d67171065e9418a2469254ea5
Fotis Alexandrou <falexandrou@gmail.com>
2023-11-02 01:59:23 +0200

    move function to operation module

--- src/core/operation.ts
+++ src/core/operation.ts
@@ -2,7 +2,6 @@ import { Registry } from '@core/registry'
 import { hashObject } from '@lib/hash'
 import { isEmpty, uniqBy } from 'lodash'
 import { validate, validateEnvironment } from '@core/validation'
-import { assertRequirementsSatisfied } from '@core/service'
 import type { Stack } from '@core/stack'
 import type {
   BaseServiceAttributes,
@@ -47,6 +46,30 @@ export const getProvisionable = (config: BaseServiceAttributes): BaseProvisionab
     registered: false,
     resourceId: getProvisionableResourceId(config),
   }
+}
+
+/**
+ * @param {BaseProvisionable} provisionable the provisionable to check
+ * @throws {Error} if a requirement is not satisfied
+ */
+export const assertRequirementsSatisfied = (provisionable: BaseProvisionable) => {
+  const {
+    service: { associations, type },
+    requirements,
+  } = provisionable
+  if (!associations) {
+    return
+  }
+
+  Object.entries(associations).forEach(([name, assoc]) => {
+    if (!assoc.requirement) {
+      return
+    }
+
+    if (!requirements[name]) {
+      throw new Error(`Requirement ${name} for service ${type} is not satisfied`)
+    }
+  })
 }
 
 export class Operation {
