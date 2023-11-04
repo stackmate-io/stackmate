ce6a478d6ce5b4a17d3fe47f155ab2b4007c94c1
Fotis Alexandrou <falexandrou@gmail.com>
2023-11-05 00:43:05 +0200

    refactor errors

--- tests/lib/errors.test.ts
+++ tests/lib/errors.test.ts
@@ -1,51 +1,49 @@
 import { faker } from '@faker-js/faker'
 import { ValidationError, ValidationErrorDescriptor, EnvironmentValidationError } from '@lib/errors'
 
-describe('errors', () => {
-  describe('ValidationError', () => {
-    let message: string
-    let subject: ValidationError
-    let errors: ValidationErrorDescriptor[]
+describe('ValidationError', () => {
+  let message: string
+  let subject: ValidationError
+  let errors: ValidationErrorDescriptor[]
 
-    beforeEach(() => {
-      message = faker.lorem.sentence()
-      errors = [
-        {
-          message: faker.lorem.sentence(),
-          path: faker.system.directoryPath(),
-        }
-      ]
+  beforeEach(() => {
+    message = faker.lorem.sentence()
+    errors = [
+      {
+        message: faker.lorem.sentence(),
+        path: faker.system.directoryPath(),
+      }
+    ]
 
-      subject = new ValidationError(message, errors)
-    })
-
-    it('provides a message', () => {
-      expect(subject.message).toEqual(message)
-    })
-
-    it('provides the errors', () => {
-      expect(subject.errors).toEqual(errors)
-    })
+    subject = new ValidationError(message, errors)
   })
 
-  describe('EnvironmentValidationError', () => {
-    let subject: EnvironmentValidationError
-    let message: string
-    let vars: string[]
+  it('provides a message', () => {
+    expect(subject.message).toEqual(message)
+  })
 
-    beforeEach(() => {
-      message = faker.lorem.sentence()
-      vars = faker.lorem.words(4).split(' ').map(w => w.toUpperCase())
+  it('provides the errors', () => {
+    expect(subject.errors).toEqual(errors)
+  })
+})
 
-      subject = new EnvironmentValidationError(message, vars)
-    })
+describe('EnvironmentValidationError', () => {
+  let subject: EnvironmentValidationError
+  let message: string
+  let vars: string[]
 
-    it('provides a message', () => {
-      expect(subject.message).toEqual(message)
-    })
+  beforeEach(() => {
+    message = faker.lorem.sentence()
+    vars = faker.lorem.words(4).split(' ').map(w => w.toUpperCase())
 
-    it('provides the variables', () => {
-      expect(subject.vars).toEqual(vars)
-    })
+    subject = new EnvironmentValidationError(message, vars)
+  })
+
+  it('provides a message', () => {
+    expect(subject.message).toEqual(message)
+  })
+
+  it('provides the variables', () => {
+    expect(subject.vars).toEqual(vars)
   })
 })
