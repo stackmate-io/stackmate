71e866b708d2aa3189c7a878f4ce2fb1f79500c5
Fotis Alexandrou <301123+falexandrou@users.noreply.github.com>
2023-11-04 16:16:44 +0200

    wip: refactor services

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,5 @@ export * from '@constants'
 export * from '@lib/errors'
 export * from '@lib/util'
 
-export { Registry as Services } from '@core/registry'
-export { Operation } from '@core/operation'
+export { Registry as Services } from '@services/registry'
+export { Operation } from './operation'
