a9cdb008e4d2e415baaa48fa5eff8408b344781b
Fotis Alexandrou <falexandrou@gmail.com>
2023-11-13 20:07:14 +0200

    refactor local provider generics

--- src/services/providers/local/utils/getLocalService.ts
+++ src/services/providers/local/utils/getLocalService.ts
@@ -1,29 +1,8 @@
 import pipe from 'lodash/fp/pipe'
-import { PROVIDER, SERVICE_TYPE } from '@src/constants'
+import { PROVIDER } from '@src/constants'
 import { withAssociations } from '@services/behaviors'
 import { getBaseService } from '@services/utils'
-import type { provider as terraformLocalProvider } from '@cdktf/provider-local'
-import type { BaseServiceAttributes, ServiceTypeChoice } from '@services/types'
-import type {
-  LocalProviderAttributes,
-  LocalProviderProvisionable,
-  LocalServiceAssociations,
-} from '@local/types'
-
-/**
- * @var {LocalServiceAssociations} associations Service Associations applied to all local services
- */
-const associations: LocalServiceAssociations = {
-  providerInstance: {
-    with: SERVICE_TYPE.PROVIDER,
-    requirement: true,
-    where: (config: LocalProviderAttributes, linked: BaseServiceAttributes) =>
-      config.provider === linked.provider,
-    handler: (p: LocalProviderProvisionable): terraformLocalProvider.LocalProvider => {
-      return p.provisions.provider
-    },
-  },
-}
+import type { ServiceTypeChoice } from '@services/types'
 
 export const getLocalService = (type: ServiceTypeChoice) =>
   pipe(withAssociations(associations))(getBaseService(PROVIDER.LOCAL, type))
