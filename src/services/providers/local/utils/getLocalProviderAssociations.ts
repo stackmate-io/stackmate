d0ed88c1cabb9c5aa62ba7c60a90716719983ed5
Fotis Alexandrou <falexandrou@gmail.com>
2023-11-13 20:07:14 +0200

    refactor local provider generics

---
+++ src/services/providers/local/utils/getLocalProviderAssociations.ts
@@ -0,0 +1,27 @@
+import { SERVICE_TYPE } from '@src/constants'
+import type { provider as terraformLocalProvider } from '@cdktf/provider-local'
+import type {
+  LocalProviderAttributes,
+  LocalProviderProvisionable,
+  LocalProviderAssociations,
+} from '@local/types'
+import type { BaseServiceAttributes } from '@services/types'
+
+import type { BaseServiceAttributes } from '@src/services/types'
+import type {
+  LocalProviderAssociations,
+  LocalProviderAttributes,
+  LocalProviderProvisionable,
+} from '../types'
+
+export const getLocalProviderAssociations = (): LocalProviderAssociations => ({
+  providerInstance: {
+    with: SERVICE_TYPE.PROVIDER,
+    requirement: true,
+    where: (config: LocalProviderAttributes, linked: BaseServiceAttributes) =>
+      config.provider === linked.provider,
+    handler: (p: LocalProviderProvisionable): terraformLocalProvider.LocalProvider => {
+      return p.provisions.provider
+    },
+  },
+})
