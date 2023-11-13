4b407d7f5644516ad9af67896c3b14b0b21fa7d1
Fotis Alexandrou <falexandrou@gmail.com>
2023-11-13 18:59:23 +0200

    aws object store service

--- src/services/providers/services.ts
+++ src/services/providers/services.ts
@@ -5,6 +5,7 @@ export { AwsSecretsVault } from './aws/services/secrets'
 export { AwsPostgreSQL, AwsMySQL, AwsMariaDB } from './aws/services/database'
 export { AwsNetworking } from './aws/services/networking'
 export { AwsRedis, AwsMemcached } from './aws/services/cache'
+export { AwsObjectStore } from './aws/services/objectStore'
 
 // Local services
 export { LocalProvider } from './local/services/provider'
