0f64642939d8e1e0170259bfd9e9b20373852de3
Fotis Alexandrou <falexandrou@gmail.com>
2023-11-13 20:12:34 +0200

    clean up unused utility

--- src/services/providers/local/utils/getLocalService.ts
+++
@@ -1,8 +0,0 @@
-import pipe from 'lodash/fp/pipe'
-import { PROVIDER } from '@src/constants'
-import { withAssociations } from '@services/behaviors'
-import { getBaseService } from '@services/utils'
-import type { ServiceTypeChoice } from '@services/types'
-
-export const getLocalService = (type: ServiceTypeChoice) =>
-  pipe(withAssociations(associations))(getBaseService(PROVIDER.LOCAL, type))
