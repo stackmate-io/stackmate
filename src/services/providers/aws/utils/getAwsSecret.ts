f050239af385690cc1dc9c02e382c6283aa161f2
Fotis Alexandrou <falexandrou@gmail.com>
2023-11-23 13:39:28 +0200

    introduce aws secrets utility

---
+++ src/services/providers/aws/utils/getAwsSecret.ts
@@ -0,0 +1,80 @@
+import crypto from 'node:crypto'
+import {
+  dataAwsSecretsmanagerRandomPassword,
+  secretsmanagerSecret,
+  secretsmanagerSecretVersion,
+} from '@cdktf/provider-aws'
+import { Fn, TerraformLocal } from 'cdktf'
+import { isNumber, isString, merge, uniq } from 'lodash'
+import { DEFAULT_PASSWORD_LENGTH, isTestMode } from '@src/constants'
+import type { kmsKey } from '@cdktf/provider-aws'
+import type { Stack } from '@src/lib/stack'
+import type { OneOfType } from '@src/lib/util'
+import type { CredentialsHandlerOptions } from '@src/services/behaviors'
+
+export const getSecret = (
+  secretName: string,
+  stack: Stack,
+  kmsKey?: kmsKey.KmsKey,
+  opts?: OneOfType<[{ value: string }, CredentialsHandlerOptions]>,
+): {
+  secret: secretsmanagerSecret.SecretsmanagerSecret
+  version: secretsmanagerSecretVersion.SecretsmanagerSecretVersion
+  value: TerraformLocal
+} => {
+  const resourceId = `secret_${secretName}_${crypto.randomBytes(8).toString('hex')}`
+
+  let value: TerraformLocal
+
+  if (opts?.value && isString(opts.value)) {
+    value = new TerraformLocal(stack.context, `${resourceId}_secret_value`, opts.value)
+  } else {
+    const excludeCharacters = uniq(merge([], opts?.exclude || [], ['/', '\\', '@', ' ']))
+    const passwordResource =
+      new dataAwsSecretsmanagerRandomPassword.DataAwsSecretsmanagerRandomPassword(
+        stack.context,
+        `${resourceId}_secret_value`,
+        {
+          passwordLength: opts?.length || DEFAULT_PASSWORD_LENGTH,
+          excludeCharacters: excludeCharacters.join(''),
+          includeSpace: false,
+          excludeNumbers: excludeCharacters.some(isNumber),
+        },
+      )
+
+    value = new TerraformLocal(
+      stack.context,
+      `${resourceId}_secret_value`,
+      passwordResource.randomPassword,
+    )
+  }
+
+  const secret = new secretsmanagerSecret.SecretsmanagerSecret(
+    stack.context,
+    `${resourceId}_secret`,
+    {
+      name: secretName,
+      kmsKeyId: kmsKey?.id,
+      provider: kmsKey?.provider,
+      recoveryWindowInDays: !isTestMode ? 30 : 0,
+    },
+  )
+
+  const version = new secretsmanagerSecretVersion.SecretsmanagerSecretVersion(
+    stack.context,
+    `${resourceId}_secret_version`,
+    {
+      secretId: secret.id,
+      secretString: Fn.jsonencode(value.toString()),
+      lifecycle: {
+        ignoreChanges: ['secret_string'],
+      },
+    },
+  )
+
+  return {
+    secret,
+    version,
+    value,
+  }
+}
