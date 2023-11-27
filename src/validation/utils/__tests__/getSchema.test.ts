ca90d44cec4799183a1c5532759baa4a0568ad14
Fotis Alexandrou <falexandrou@gmail.com>
2023-11-27 17:56:01 +0200

    chore: refactor tests

---
+++ src/validation/utils/__tests__/getSchema.test.ts
@@ -0,0 +1,40 @@
+import { Registry } from '@src/services/registry'
+import { getAjv } from '@src/validation/utils/getAjv'
+import { getSchema } from '@src/validation/utils/getSchema'
+import type { ServiceAttributes } from '@src/services/registry'
+import type { JsonSchema } from '@src/lib/schema'
+
+describe('getSchema', () => {
+  let schema: JsonSchema<ServiceAttributes[]>
+  beforeAll(() => {
+    schema = getSchema()
+  })
+
+  it('returns a valid schema', () => {
+    const ajv = getAjv()
+    const schemaId = schema.$id
+
+    if (!schemaId) {
+      throw new Error('')
+    }
+
+    expect(() => ajv.validateSchema(schema, true)).not.toThrow()
+    expect(ajv.errors).toBeNull()
+  })
+
+  it('requires that the schema is an array of objects', () => {
+    expect(schema).toMatchObject({
+      $id: 'stackmate-services-configuration',
+      type: 'array',
+      minItems: 1,
+      items: expect.objectContaining({
+        type: 'object',
+        required: expect.arrayContaining(['name', 'type', 'provider']),
+      }),
+    })
+
+    expect(Object.keys(schema.$defs || {})).toEqual(
+      expect.arrayContaining(Registry.all().map((srv) => srv.schemaId)),
+    )
+  })
+})
