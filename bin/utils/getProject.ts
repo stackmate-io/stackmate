84211ece715b7b0a049beae2627554a09cbd1ba6
Fotis Alexandrou <falexandrou@gmail.com>
2023-12-02 01:38:33 +0200

    fix type conflicts

--- bin/utils/getProject.ts
+++ bin/utils/getProject.ts
@@ -2,14 +2,14 @@ import { Operation } from '@src/operation'
 import { Project } from '@src/project'
 import { ENVIRONMENT } from '@src/project/constants'
 import { getProjectServices } from '@src/project/utils/getProjectServices'
-import type { ProjectConfiguration } from '@src/project'
+import type { EnvironmentChoice, ProjectConfiguration } from '@src/project'
 import type { CdktfProjectOptions } from '@cdktf/cli-core/src/lib/cdktf-project'
 
 type GetProjectOpts = Partial<Pick<CdktfProjectOptions, 'workingDirectory' | 'onUpdate' | 'onLog'>>
 
 export const getProject = (
   configuration: ProjectConfiguration,
-  environment: string = ENVIRONMENT.PRODUCTION,
+  environment: EnvironmentChoice = ENVIRONMENT.PRODUCTION,
   { workingDirectory, onUpdate, onLog }: GetProjectOpts = {},
 ): Project => {
   const services = getProjectServices(configuration, environment)
